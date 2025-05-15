c065d31a184df61271a32ce67ce6a9d98f589cfa
Lucas Zenha <lucaszenh@gmail.com>
2025-05-15 18:39:14 +0000

    Fix this UI also

--- src/components/agenda/appointment-form.tsx
+++ src/components/agenda/appointment-form.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import type { AppointmentFormData, Appointment } from "@/lib/schemas";
@@ -12,7 +13,6 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-  FormDescription,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
